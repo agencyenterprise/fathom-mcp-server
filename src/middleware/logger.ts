3118acb51f7bf44f0f6ee1d867fb984589eb24d6
jpatterson933 <jeff@ae.studio>
2026-01-28 23:39:30 -0800

    Enhance request logger middleware with TypeScript typings
    - Added ServerResponse type to the request logger for improved type safety.
    - Updated genReqId function to specify return type as ReqId.
    - Refactored customLogLevel function to include type annotations for parameters, enhancing code clarity and maintainability.

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,7 +1,7 @@
 import { randomUUID } from "crypto";
-import { IncomingMessage } from "http";
+import { IncomingMessage, ServerResponse } from "http";
 import pino from "pino";
-import pinoHttp from "pino-http";
+import pinoHttp, { ReqId } from "pino-http";
 import { config } from "../common/config";
 
 export const logger = pino({
@@ -14,7 +14,7 @@ export const logger = pino({
 
 export const requestLogger = pinoHttp({
   logger,
-  genReqId: (req: IncomingMessage) =>
+  genReqId: (req: IncomingMessage): ReqId =>
     (req.headers["x-railway-request-id"] as string) ||
     (req.headers["x-request-id"] as string) ||
     randomUUID(),
@@ -22,16 +22,16 @@ export const requestLogger = pinoHttp({
     req: (req) => ({
       method: req.method,
       url: req.url,
-      ...(req.query &&
-        Object.keys(req.query).length > 0 && { query: req.query }),
-      ...(req.params &&
-        Object.keys(req.params).length > 0 && { params: req.params }),
     }),
     res: (res) => ({
       statusCode: res.statusCode,
     }),
   },
-  customLogLevel: (_req, res, err) => {
+  customLogLevel: (
+    _req: IncomingMessage,
+    res: ServerResponse,
+    err: Error | undefined,
+  ) => {
     if (res.statusCode >= 400 || err) return "error";
     return "info";
   },
